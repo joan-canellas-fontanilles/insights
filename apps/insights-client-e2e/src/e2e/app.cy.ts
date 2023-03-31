60d35b7e7bc1f3bb6916b7a80b1dda0e23429e49
Joan Cañellas Fontanilles <joan.canellas.fontanilles@gmail.com>
2023-03-31 14:25:42 +0200

    test: Insights-client-e2e - Remove unused template files

--- apps/insights-client-e2e/src/e2e/app.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('insights-client', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should be redirected to dashboard', () => {
-    cy.location('pathname').should('eq', '/dashboard');
-  });
-});
